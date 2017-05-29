895d959c8858a1e2a8c5dfe6a06c188cc654f093
Lawrence <lawrenceevalyn@gmail.com>
2017-05-29 18:30:18 -0400

    set up game to run in terminal with game.py

---
+++ game/game/game.py
@@ -0,0 +1,38 @@
+import map
+import lexicon
+import parser
+import inventory
+
+
+# this is what will run the game
+
+class GameEngine(object):
+
+    def __init__(self, current_room):
+        self.current_room = current_room
+    
+    def play(self):
+        while self.current_room != exit:
+            print self.current_room.description
+        
+            player_input = raw_input("> ")
+            
+            if player_input == "Q":
+                print "Goodbye"
+                self.current_room = exit
+            
+            elif player_input == 'north' or 'south' or 'east' or 'west' :
+                if self.current_room.go(player_input) == None:
+                    print "You can't go that way."
+                else:
+                    self.next_room = self.current_room.go(player_input)
+                    print self.next_room.name
+                    self.current_room = self.next_room
+        
+        print self.current_room.description
+
+
+# let's run the game now!
+
+a_game = GameEngine(entrance)
+a_game.play()
--- game/tests/app_tests.py
+++
@@ -1,4 +0,0 @@
-from nose.tools import *
-from bin.app import app
-from tests.tools import assert_response
-
