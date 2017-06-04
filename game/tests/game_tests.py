8cad47dd81ba2b6c728bdfeedfb1c0306e5a74a8
Lawrence <lawrenceevalyn@gmail.com>
2017-06-03 22:21:29 -0400

    set up game.py tests

---
+++ game/tests/game_tests.py
@@ -0,0 +1,9 @@
+from nose.tools import *
+from gameweb import lexicon
+
+
+def test_engine():
+# some kind of test to confirm the game engine works?
+
+def text_inputs():
+# some kind of test to confirm the file processes inputs properly?
