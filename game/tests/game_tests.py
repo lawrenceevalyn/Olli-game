9f579bbdd954f334db5aa8bb1c3ef4af679e6209
Lawrence <lawrenceevalyn@gmail.com>
2017-06-16 19:22:59 -0700

    planned tests

--- game/tests/game_tests.py
+++ game/tests/game_tests.py
@@ -3,7 +3,11 @@ from gameweb import lexicon
 
 
 def test_engine():
-# some kind of test to confirm the game engine works?
+# test that N, S, E, W correctly move the player to the next room
+# test that if there's no room it says "You can't go that way."
 
 def text_inputs():
 # some kind of test to confirm the file processes inputs properly?
+# right now the program doesn't really parse inputs, but later it will
+# (and at that time I will want to test that it hands things off to the
+# parser correctly)
