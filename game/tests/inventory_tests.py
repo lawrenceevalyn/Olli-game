ec6de388c6fabdbfd3372a4dcfa95a57ba4ee7c5
Lawrence <lawrenceevalyn@gmail.com>
2017-07-20 02:12:08 -0400

    skeleton for inventory_tests

---
+++ game/tests/inventory_tests.py
@@ -0,0 +1,7 @@
+from nose.tools import *
+from game import inventory
+
+def test_items():
+    # something testing that an item has the right description
+    
+    # something testing that the items start out in the right rooms
