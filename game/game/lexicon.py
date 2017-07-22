5fefdc6e908b6128b889ca2a26cc0893b663d0b3
Lawrence <lawrenceevalyn@gmail.com>
2017-07-22 17:18:28 -0400

    added more inventory

--- game/game/lexicon.py
+++ game/game/lexicon.py
@@ -21,9 +21,11 @@ def convert_number(word):
 
 # this function compares a word against the lexicon
 def parse_word(word):
+
 # check that there's actually a word
     if word == None:
         return ('error', word)
+
 # parse the directions
     elif word in ["north", "North", "N", "n"]:
         return ('direction', 'north')
@@ -33,6 +35,7 @@ def parse_word(word):
         return ('direction', 'east')
     elif word in ["west", "West", "W", "w"]:
         return ('direction', 'west')
+
 # parse the verbs
     elif word in ["go", "Go", "run", "Run", "walk", "Walk", "move", "Move"]:
         return ('verb', 'go')
@@ -44,6 +47,7 @@ def parse_word(word):
         return ('verb', 'drop')
     elif word in ["look", "Look", "examine", "Examine"]:
         return ('verb', 'look')
+
 # parse the nouns
     elif word in ["room", "Room"]:
         return ('noun', 'room')
@@ -64,7 +68,16 @@ def parse_word(word):
     elif word in ["cable", "Cable"]:
         return ('noun', 'cable')
     elif word in ["mousepad", "Mousepad"]:
-        return ('noun', 'mousepad')
+        return ('noun', 'mousepad
+    elif word in ["trash", "Trash"]:
+        return ('noun', 'trash can')')
+    elif word in ["water", "Water", "spill", "Spill", "puddle", "Puddle"]:
+        return ('noun', 'water')
+    elif word in ["paper", "Paper", "towel", "Towel", "towels", "Towels", "roll", "roll"]:
+        return ('noun', 'paper towels')
+    elif word in ["card", "Card"]:
+        return ('noun', 'library card')
+    
 # parse the stopwords
     elif word in ["the", "The"]:
         return ('stop', 'the')
@@ -76,8 +89,11 @@ def parse_word(word):
         return ('stop', 'at')
     elif word in ['around', 'Around']:
         return ('stop', 'around')
-    elif word in ["barcode", "Barcode",]:
-        return ('stop', 'barcode')
+    elif word in ["barcode", "Barcode",]: # "stop" adjectives too
+        return ('stop', 'barcode')        # since I won't parse them
+    elif word in ["library", "Library"]:
+        return ('stop', 'library')
+
 # check if it's a number, and if not, give up
     else:
        return convert_number(word)
